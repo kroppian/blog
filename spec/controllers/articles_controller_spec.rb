edb7f3467a8713bc548dee0f8fe5419427ee521f
Ian Kropp <iankropp@gmail.com>
2016-04-12 21:44:49 -0400

    Made random bug fixes and increased spec coverage.

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -46,14 +46,12 @@ RSpec.describe ArticlesController, type: :controller do
     context "user not logged in and tries to view article" do
 
       it "Articles should be returned" do
-        log_in(@admin_user)
         get :index
         
         @articles.each do |expected_art|
           # TODO tidy up 
           expect(assigns(:articles).select {|found_art| found_art[:id] == expected_art.id }.first.id).to eql(expected_art.id)
         end
-        log_out
       end
 
     end  
@@ -79,13 +77,131 @@ RSpec.describe ArticlesController, type: :controller do
 
     context "user not logged in and tires to access new article page" do
 
-    end
+      it "returns a 403 error" do
 
-  end
+        get :new
+        expect(response.response_code).to eql(403)
+
+      end
+
+    end # end -- user not logged in and tires to access new article page
+
+    context "non-admin user logged in and tires to access new article page" do
+
+      it "returns a 403 error" do
+
+        log_in(@normal_user)
+        get :new
+        expect(response.response_code).to eql(403)
+        log_out
+
+      end
+
+    end # end -- non-admin user logged in and tires to access new article page
+
+    context "admin user logged in and tires to access new article page" do
+
+      it "returns a 200 successful" do
+
+        log_in(@admin_user)
+        get :new
+        expect(response.response_code).to eql(200)
+        log_out
+
+      end
+
+    end # end -- admin user logged in and tires to access new article page
+
+    context "owern logged in and tires to access new article page" do
+
+      it "returns a 200 successful" do
+
+        log_in(@owner_user)
+        get :new
+        expect(response.response_code).to eql(200)
+        log_out
+
+      end
+
+    end # end -- admin user logged in and tires to access new article page
+
+  end # end -- get #new
 
   describe "GET #edit" do
 
-  end
+    context "user not logged in and tires to access edit article page" do
+
+      it "returns a 403 error" do
+
+        @articles.each do |art| 
+
+          get :edit, id: art.id
+          expect(response.response_code).to eql(403)
+
+        end
+
+      end
+
+    end # end -- user not logged in and tires to access edit article page
+
+    context "non-admin user tires to access edit article page" do
+
+      it "returns a 403 error" do
+
+        log_in @normal_user
+
+        @articles.each do |art| 
+
+          get :edit, id: art.id
+          expect(response.response_code).to eql(403)
+
+        end
+
+        log_out
+
+      end
+
+    end # end -- non-admin user tires to access edit article page
+
+    context "admin user tires to access edit article page" do
+
+      it "returns a 200 error" do
+
+        log_in @admin_user
+
+        @articles.each do |art| 
+
+          get :edit, id: art.id
+          expect(response.response_code).to eql(200)
+
+        end
+
+        log_out
+
+      end
+
+    end # end -- admin user tires to access edit article page
+
+    context "owner tires to access edit article page" do
+
+      it "returns a 200 error" do
+
+        log_in @admin_user
+
+        @articles.each do |art| 
+
+          get :edit, id: art.id
+          expect(response.response_code).to eql(200)
+
+        end
+
+        log_out
+
+      end
+
+    end # end -- owner tires to access edit article page
+
+  end # end -- GET #edit
 
   describe "POST #create" do
 
