293aeb10241233008b179f2698c5c1bae4388b22
Ian Kropp <iankropp@gmail.com>
2016-04-17 14:20:32 -0400

    Added specs for the Article controller and cleaned up the format of User specs.

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,103 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'title format' do 
+
+    context 'when Article is proposed with nil title' do 
+      it 'should raise an active record error' do 
+        new_article = build(:article, title: nil)  
+        expect{ new_article.save! }.to raise_error(ActiveRecord::RecordInvalid)
+      end 
+    end
+
+    context 'when Article is proposed with title of length zero' do 
+      it 'should raise an active record error' do 
+        new_article = build(:article, title: '')  
+        expect{ new_article.save! }.to raise_error(ActiveRecord::RecordInvalid)
+      end 
+    end
+
+    context 'when Article is proposed with with title of length of one' do 
+      it 'should create the proposed article' do 
+        new_article = build(:article, title: 't')  
+        expect{ new_article.save! }.to change {Article.count}
+      end 
+    end
+
+    context 'when Article is proposed with title of length greater than one' do 
+      it 'should create the proposed article' do 
+        new_article = build(:article, title: 'This is a title')  
+        expect{ new_article.save! }.to change {Article.count}
+      end 
+    end
+
+    context 'when Article is proposed with title of length 120 chars' do 
+      it 'should create the proposed article' do 
+        new_article = build(:article, title: 'a' * 120)
+        expect{ new_article.save! }.to change {Article.count}
+      end 
+    end
+
+    context 'when Article is proposed with title of length greater than 120 chars' do 
+      it 'should create the proposed article' do 
+        new_article = build(:article, title: 'a' * 121)
+        expect{ new_article.save! }.to raise_error(ActiveRecord::RecordInvalid)
+      end 
+    end
+
+
+  end
+
+  describe 'text format' do 
+
+    context 'when Article is proposed with nil text' do 
+      it 'should raise an active record error' do 
+        new_article = build(:article, text: nil)  
+        expect{ new_article.save! }.to raise_error(ActiveRecord::RecordInvalid)
+      end 
+    end
+
+    context 'when Article is proposed with text of length zero' do 
+      it 'should raise an active record error' do 
+        new_article = build(:article, text: '')  
+        expect{ new_article.save! }.to raise_error(ActiveRecord::RecordInvalid)
+      end 
+    end
+
+    context 'when Article is proposed with text of length one' do 
+      it 'should create the proposed article' do 
+        new_article = build(:article, text: 'a')  
+        expect{ new_article.save! }.to change {Article.count}
+      end 
+    end
+
+  end
+
+  describe 'user id' do 
+
+    context 'when Article is proposed with nil user_id' do 
+      it 'should raise an active record error' do 
+        new_article = build(:article, user_id: nil)  
+        expect{ new_article.save! }.to raise_error(ActiveRecord::RecordInvalid)
+      end 
+    end
+
+    context 'when Article is proposed with non-existant user_id' do 
+      it 'should raise an active record error' do 
+        new_article = build(:article, user_id: -1)  
+        expect{ new_article.save! }.to raise_error(ActiveRecord::RecordInvalid)
+      end 
+    end
+
+
+    context 'when Article is proposed with existing user_id' do 
+      it 'should raise an active record error' do 
+        new_article = build(:article)  
+        expect{ new_article.save! }.to change {Article.count}
+      end 
+    end
+
+
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,17 +4,18 @@ RSpec.describe User, type: :model do
 
   # TODO should I use 'it_behaves_like? 
   # TODO should I use vcr?
+  # TODO should I tests updates too? 
 
   describe 'user_type value' do 
 
-    context 'a user tries to create a user without a user_type' do 
+    context 'when a User is proposed without a user_type' do 
       it 'it should raise an active record error' do 
         new_user = build(:user, user_type: nil)  
         expect{ new_user.save! }.to raise_error(ActiveRecord::RecordInvalid)
       end
     end
     
-    context 'a user tries to create a user with an out of range user_type' do 
+    context 'when a User is proposed with an out of range user_type' do 
       it 'it should raise an active record error' do 
         # TODO how can I properly test this if I can't even use FactoryGirl to create
         # and draft user?
@@ -27,21 +28,21 @@ RSpec.describe User, type: :model do
 
   describe 'about page' do 
 
-    context 'user tries to create a user with a nil about' do 
+    context 'when a User is proposed with a nil about' do 
       it 'should create the user' do 
         new_user = build(:user, about: nil)
         expect{ new_user.save! }.to change {User.count}
       end
     end
 
-    context 'user tries to create a user with a blank string' do 
+    context 'when a User is proposed with a blank string' do 
       it 'should create the user' do 
         new_user = build(:user, about: '')
         expect{ new_user.save! }.to change {User.count}
       end
     end
 
-    context 'user tries to create a user with a non-zero string' do 
+    context 'when a User is proposed with a non-zero string' do 
       it 'should create the user' do 
         new_user = build(:user, about: 'I\'m a dude.')
         expect{ new_user.save! }.to change {User.count}
@@ -53,7 +54,7 @@ RSpec.describe User, type: :model do
   
   describe 'name formatting' do 
 
-    context 'user tries to create a user with a name of length zero' do 
+    context 'when a User is proposed with a name of length zero' do 
       # TODO what the hell are these? Are they functions?
       it 'should raise an active record error' do 
         new_user = build(:user, name: '')
@@ -61,15 +62,15 @@ RSpec.describe User, type: :model do
       end
     end
 
-    context 'user tries to create a user with a name of nil' do 
+    context 'when a User is proposed with a name of nil' do 
       it 'should raise an active record error' do 
         new_user = build(:user, name: nil)
         expect{ new_user.save! }.to raise_error(ActiveRecord::RecordInvalid)
       end
     end 
 
-    context 'user tries to create a user with a name of one character' do 
-      it 'should raise an active record error' do 
+    context 'when a User is proposed with a name of one character' do 
+      it 'should create the user correctly' do 
         new_user = build(:user, name: 'Q')
         expect{ new_user.save! }.to change {User.count}
       end
@@ -81,35 +82,35 @@ RSpec.describe User, type: :model do
 
   describe 'password formatting' do 
 
-    context 'user tries to create a user with a nil password' do 
+    context 'when a User is proposed with a nil password' do 
        it 'should raise an active record error' do 
         new_user = build(:user, password: nil, password_confirmation: nil)
         expect{ new_user.save! }.to raise_error(ActiveRecord::RecordInvalid)
       end
     end
 
-    context 'user tries to create a user with a password of length zero' do 
+    context 'when a User is proposed with a password of length zero' do 
        it 'should raise an active record error' do 
         new_user = build(:user, password: '', password_confirmation: '')
         expect{ new_user.save! }.to raise_error(ActiveRecord::RecordInvalid)
       end
     end
 
-    context 'user tries to create a user with a password with a length less than 6' do 
+    context 'when a User is proposed with a password with a length less than 6' do 
        it 'should raise an active record error' do 
         new_user = build(:user, password: '12345', password_confirmation: '12345')
         expect{ new_user.save! }.to raise_error(ActiveRecord::RecordInvalid)
       end
     end
 
-    context 'user tries to create a user with a password with a length of 6' do 
+    context 'when a User is proposed with a password with a length of 6' do 
        it 'should create the user correctly' do 
         new_user = build(:user, password: '123456', password_confirmation: '123456')
         expect{ new_user.save! }.to change {User.count}
       end
     end
 
-    context 'user tries to create a user with a password with incorrect password confirmation' do 
+    context 'when a User is proposed with a password with incorrect password confirmation' do 
        it 'should raise an active record error' do 
         new_user = build(:user, password: '123456', password_confirmation: '223456')
         expect{ new_user.save! }.to raise_error(ActiveRecord::RecordInvalid)
@@ -125,14 +126,14 @@ RSpec.describe User, type: :model do
     end
 
 
-    context 'a user tries to create user with existing email' do 
+    context 'when a User is proposed with existing email' do 
       it 'should raise an active record error' do
         new_user = build(:user, email: 'joe@schmoe.net')
         expect{ new_user.save! }.to raise_error(ActiveRecord::RecordInvalid)
       end
     end 
 
-    context 'a user tries to create a user with unique email' do
+    context 'when a User is proposed with unique email' do
       it 'should create the new user' do 
         new_user = build(:user, email: 'howard_schmoe.21@schmoe.net')
         expect{ new_user.save! }.to change {User.count}
@@ -143,70 +144,70 @@ RSpec.describe User, type: :model do
 
   describe 'email formatting' do 
 
-    context 'a user tries to create a blank user email' do
+    context 'when a User is proposed with a blank email' do
       it 'should raise an active record error' do 
         new_user = build(:user, email: '')
         expect { new_user.save! }.to raise_error(ActiveRecord::RecordInvalid)
       end 
     end 
 
-    context 'a user tries to create a user email without an at sign' do
+    context 'when a User is proposed without an at sign' do
       it 'should raise an active record error' do 
         new_user = build(:user, email: 'heythere.net')
         expect {new_user.save! }.to raise_error(ActiveRecord::RecordInvalid)
       end 
     end 
 
-    context 'a user tries to create a user email without domain type' do
+    context 'when a User is proposed without domain type' do
       it 'should raise an active record error' do 
         new_user = build(:user, email: 'bob@slob')
         expect {new_user.save! }.to raise_error(ActiveRecord::RecordInvalid)
       end 
     end 
 
-    context 'a user tries to create a user email with one character local name' do
-      it 'should raise an active record error' do 
+    context 'when a User is proposed with a one character local name' do
+      it 'should create the user correctly' do 
         new_user = build(:user, email: 'b@cool.io')
         expect {new_user.save! }.to change {User.count}
       end 
     end 
 
-    context 'a user tries to create a user email with no local name' do
+    context 'when a User is proposed with no local name' do
       it 'should raise an active record error' do 
         new_user = build(:user, email: '@cool.io')
         expect {new_user.save! }.to raise_error(ActiveRecord::RecordInvalid)
       end 
     end 
 
-    context 'a user tries to create a user email with no domain name' do
+    context 'when a User is proposed with an email with no domain name' do
       it 'should raise an active record error' do 
         new_user = build(:user, email: 'nobody@')
         expect {new_user.save! }.to raise_error(ActiveRecord::RecordInvalid)
       end 
     end 
 
-    context 'a user tries to create a user email with no domain name save a domain type' do
+    context 'when a User is proposed with a user email with no domain name save a domain type' do
       it 'should raise an active record error' do 
         new_user = build(:user, email: 'nobody@.net')
         expect {new_user.save! }.to raise_error(ActiveRecord::RecordInvalid)
       end 
     end 
 
-    context 'a user tries to create a user email with local name starting with a period' do
+    context 'when a User is proposed with a user email with local name starting with a period' do
       it 'should raise an active record error' do 
         new_user = build(:user, email: '.dottydotdot@dot.org')
         expect {new_user.save! }.to raise_error(ActiveRecord::RecordInvalid)
       end 
     end 
 
-    context 'a user tries to create a user email with local name end with a period' do
+    context 'when a User is proposed with a user email with a local name that ends with a period' do
       it 'should raise an active record error' do 
         new_user = build(:user, email: 'dottydotdot.@dot.org')
         expect {new_user.save! }.to raise_error(ActiveRecord::RecordInvalid)
       end 
     end 
 
-    context 'a user tries to create a user email with local name just a period' do
+    context 'when a User is proposed with a user email with local name just a period' do
       it 'should raise an active record error' do 
         new_user = build(:user, email: '.@dot.org')
         expect {new_user.save! }.to raise_error(ActiveRecord::RecordInvalid)
