12e36f2c03daf44b03095b80efdc68a8d5b0b1de
Ian Kropp <iankropp@gmail.com>
2016-05-04 00:03:03 -0400

    Added specs for the create method in the article controller.

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe ArticlesController, type: :controller do
 
+  let(:article) { article.create(title: 'Assignment', text:'blah blah') }
+
   before do 
 
     @normal_user = create(:user)    
@@ -14,18 +16,18 @@ RSpec.describe ArticlesController, type: :controller do
 
   end
  
-  describe "GET #show" do
+  describe 'GET #show' do
 
-    context "user not logged in and tries to view article" do
-      it "Article should be displayed" do
+    context 'user not logged in and tries to view article' do
+      it 'Article should be displayed' do
         get :show, id: @articles.first.id 
         expect(assigns(:article).id).to eql(@articles.first.id)
         expect(response.response_code).to eql(200)
       end
     end  
 
-    context "non-admin user logs in and tries to view article" do
-      it "Article should be displayed" do
+    context 'non-admin user logs in and tries to view article' do
+      it 'Article should be displayed' do
         log_in(@normal_user)
         get :show, id: @articles.first.id 
         expect(assigns(:article).id).to eql(@articles.first.id)
@@ -34,8 +36,8 @@ RSpec.describe ArticlesController, type: :controller do
       end
     end  
 
-    context "admin user logs in and tries to view article" do
-      it "Article should be displayed" do
+    context 'admin user logs in and tries to view article' do
+      it 'Article should be displayed' do
         log_in(@admin_user)
         get :show, id: @articles.first.id 
         expect(assigns(:article).id).to eql(@articles.first.id)
@@ -46,10 +48,10 @@ RSpec.describe ArticlesController, type: :controller do
 
   end 
 
-  describe "GET #index" do
+  describe 'GET #index' do
 
-    context "user not logged in and tries to view article" do
-      it "Articles should be returned" do
+    context 'user not logged in and tries to view article' do
+      it 'Articles should be returned' do
         get :index
         @articles.each do |expected_art|
           # TODO tidy up 
@@ -58,8 +60,8 @@ RSpec.describe ArticlesController, type: :controller do
       end
     end  
 
-    context "user logs in and tries to view article" do
-      it "Articles should be returned" do
+    context 'user logs in and tries to view article' do
+      it 'Articles should be returned' do
         log_in(@admin_user)
         get :index
         @articles.each do |expected_art|
@@ -72,18 +74,18 @@ RSpec.describe ArticlesController, type: :controller do
 
   end
 
-  describe "GET #new" do
+  describe 'GET #new' do
 
-    context "user not logged in and tries to access new article page" do
-      it "returns a 403 error" do
+    context 'user not logged in and tries to access new article page' do
+      it 'returns a 403 error' do
         get :new
         expect(response.response_code).to eql(403)
         expect(assigns(:article)).to eql(nil)
       end
     end 
 
-    context "non-admin user logged in and tires to access new article page" do
-      it "returns a 403 error" do
+    context 'non-admin user logged in and tires to access new article page' do
+      it 'returns a 403 error' do
         log_in(@normal_user)
         get :new
         expect(response.response_code).to eql(403)
@@ -92,8 +94,8 @@ RSpec.describe ArticlesController, type: :controller do
       end
     end 
 
-    context "admin user logged in and tires to access new article page" do
-      it "returns a 200 successful" do
+    context 'admin user logged in and tires to access new article page' do
+      it 'returns a 200 successful' do
         log_in(@admin_user)
         get :new
         expect(response.response_code).to eql(200)
@@ -102,8 +104,8 @@ RSpec.describe ArticlesController, type: :controller do
       end
     end 
 
-    context "owern logged in and tires to access new article page" do
-      it "returns a 200 successful" do
+    context 'owern logged in and tires to access new article page' do
+      it 'returns a 200 successful' do
         log_in(@owner_user)
         get :new
         expect(response.response_code).to eql(200)
@@ -114,10 +116,10 @@ RSpec.describe ArticlesController, type: :controller do
 
   end 
 
-  describe "GET #edit" do
+  describe 'GET #edit' do
 
-    context "user not logged in and tires to access edit article page" do
-      it "returns a 403 error" do
+    context 'user not logged in and tires to access edit article page' do
+      it 'returns a 403 error' do
         @articles.each do |art| 
           get :edit, id: art.id
           expect(response.response_code).to eql(403)
@@ -126,8 +128,8 @@ RSpec.describe ArticlesController, type: :controller do
       end
     end 
 
-    context "non-admin user tires to access edit article page" do
-      it "returns a 403 error" do
+    context 'non-admin user tires to access edit article page' do
+      it 'returns a 403 error' do
         log_in @normal_user
         @articles.each do |art| 
           get :edit, id: art.id
@@ -138,8 +140,8 @@ RSpec.describe ArticlesController, type: :controller do
       end
     end 
 
-    context "admin user tires to access edit article page" do
-      it "returns a 200 error" do
+    context 'admin user tires to access edit article page' do
+      it 'returns a 200 error' do
         log_in @admin_user
         @articles.each do |art| 
           get :edit, id: art.id
@@ -150,8 +152,8 @@ RSpec.describe ArticlesController, type: :controller do
       end
     end 
 
-    context "owner tires to access edit article page" do
-      it "returns a 200 error" do
+    context 'owner tires to access edit article page' do
+      it 'returns a 200 error' do
         log_in @admin_user
         @articles.each do |art| 
           get :edit, id: art.id
@@ -164,21 +166,89 @@ RSpec.describe ArticlesController, type: :controller do
 
   end 
 
-  describe "POST #create" do
+  describe 'POST #create' do
+
+    context 'A non logged in user tries to create an article'  do 
+      it 'Does not update the article model' do
+        expect do 
+          post :create, article: attributes_for(:article)
+        end.to_not change {Article.count}
+      end
+    end
+
+    context 'A non-admin in user tries to create an article'  do 
+      it 'Does not update the article model' do
+        log_in @normal_user
+        expect do 
+          post :create, article: attributes_for(:article)
+        end.to_not change {Article.count}
+      end
+    end
+
+    context 'An admin in user tries to create an article'  do 
+      it 'Does not update the article model' do
+        log_in @admin_user
+        expect do 
+          post :create, article: attributes_for(:article)
+        end.to change {Article.count}
+      end
+    end
+
+    context 'The owner tries to create an article'  do 
+      it 'Does not update the article model' do
+        log_in @owner_user
+        expect do 
+          post :create, article: attributes_for(:article)
+        end.to change {Article.count}
+      end
+
+    end
+
+  end
+
+  describe 'PATCH #update' do
 
     # TODO complete me
 
   end
 
-  describe "PATCH #update" do
+  describe 'POST #delete' do
 
-    # TODO complete me
+    context 'A non logged in user tries to create an article'  do 
+      it 'Does not update the article model' do
+        expect do 
+          post :create, article: attributes_for(:article)
+        end.to_not change {Article.count}
+      end
+    end
 
-  end
+    context 'A non-admin in user tries to create an article'  do 
+      it 'Does not update the article model' do
+        log_in @normal_user
+        expect do 
+          post :create, article: attributes_for(:article)
+        end.to_not change {Article.count}
+      end
+    end
 
-  describe "POST #delete" do
+    context 'An admin in user tries to create an article'  do 
+      it 'Does not update the article model' do
+        log_in @admin_user
+        expect do 
+          post :create, article: attributes_for(:article)
+        end.to change {Article.count}
+      end
+    end
 
-    # TODO complete me
+    context 'The owner tries to create an article'  do 
+      it 'Does not update the article model' do
+        log_in @owner_user
+        expect do 
+          post :create, article: attributes_for(:article)
+        end.to change {Article.count}
+      end
+
+    end
     
   end
 
